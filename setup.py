90de5d773b1a995cd5d1be03b26e4df6bbc441e8
Herman <35882542+winterhotlatte@users.noreply.github.com>
2020-02-21 17:37:24 +0900

    Bump version: 0.2.0 → 0.2.dev0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme:
 	setup(
 		name="absolute-import",
-		version="0.2.0",
+		version="0.2.dev0",
 		author="cpuabuse.com",
 		author_email="pypi@cpuabuse.com",
 		description="A package to automate setting up absolute imports and namespace.",
